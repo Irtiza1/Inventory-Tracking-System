dff2212c27507e778acd6bbda82c0b72f142e49c
Irtiza1 <irtiza.s2918@gmail.com>
2025-04-13 00:38:11 +0500

    updated the name

--- inventory-tracking-system-v2/src/routes/ReportRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportController = require('../controllers/ReportController');
-
-router.get('/inventory/:storeId', reportController.getStoreInventoryReport);
-router.get('/sales/:storeId', reportController.getSalesReport);
-
-module.exports = router;
